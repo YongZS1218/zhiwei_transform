59c1839b6e0f9ad0f85fb31fe3c17c32b4bd1c3d
YongZS永值生 <yongzs1218@qq.com>
2025-04-10 00:16:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let translateValue = 165; // 預設值
 
     if (text.length === 1) {
-      translateValue = 320; // 單字平移較多
+      translateValue = 360; // 單字平移較多
     } else if (text.length === 2) {
       translateValue = 160; // 雙字平移較少
     } else if (text.length === 0) {
